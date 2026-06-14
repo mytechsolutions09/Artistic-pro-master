a93a989d63326767af84d501ac61be8f1bc6c86c
Arpit Kanotra <arpitkanotra@ymail.com>
2026-06-14 17:32:37 +0530

    chore: remove next-sitemap package

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteUrl: 'https://lurevi.in',
-  generateRobotsTxt: true,
-  changefreq: 'weekly',
-  priority: 0.7,
-  exclude: ['/sign-in', '/cart', '/favorites', '/admin*'],
-}
